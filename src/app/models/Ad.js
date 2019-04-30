fa59537af85804abbde73abdd42028bd059d16aa
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-30 07:29:25 -0400

    Adding AdSchema

---
+++ src/app/models/Ad.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Ad = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Ad', Ad)
