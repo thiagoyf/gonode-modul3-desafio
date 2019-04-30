8b3dbb7058a58baa52ec68dd14f71cbe877a4019
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-30 14:15:43 -0400

    Adding filter to Ad

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -3,20 +3,35 @@ const Ad = require('../models/Ad')
 class AdController {
   // Metodo para listagem
   async index (req, res) {
+    const filters = {}
+
+    if (req.query.price_min || req.query.price_max) {
+      filters.price = {}
+
+      if (req.query.price_min) {
+        filters.price.$gte = req.query.price_min
+      }
+
+      if (req.query.price_max) {
+        filters.price.$lte = req.query.price_max
+      }
+    }
+
+    if (req.query.title) {
+      filters.title = new RegExp(req.query.title, 'i')
+    }
+
     // - Troca o find por paginate para trazer paginado
     // o primeiro parametro de paginate seriam os filtros passado para ele
     // - O populate insere informações de um relacionamente
     // nesse exemplo, Ad tem author que é um User
     // o author de Ad vai receber todas informações do usuário correspondente
-    const ads = await Ad.paginate(
-      {},
-      {
-        page: req.query.page || 1,
-        limit: 20,
-        populate: ['author'],
-        sort: '-createdAt'
-      }
-    )
+    const ads = await Ad.paginate(filters, {
+      page: req.query.page || 1,
+      limit: 20,
+      populate: ['author'],
+      sort: '-createdAt'
+    })
 
     return res.json(ads)
   }
