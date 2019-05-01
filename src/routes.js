945bc36b04055f9de2809335c63adfdf8f44d9e5
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-01 13:20:43 -0400

    Adding Exception Handling

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const validate = require('express-validation')
 
+// - Por padrão o as function async/promises não disparam erro, para
+// disparar erros teria  que envolver o código dentro do da função assíncrona
+// e not catch, retornar o next passando o erro: 'return next(err)'
+// - A biblioteca handle, permite que você envie o erro que acontece dentro das
+// promises sem ter que utilizar o try/catch
+const handle = require('express-async-handler')
+
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
@@ -11,12 +18,12 @@ const validators = require('./app/validators')
 routes.post(
   '/users',
   validate(validators.User),
-  controllers.UserController.store
+  handle(controllers.UserController.store)
 )
 routes.post(
   '/sessions',
   validate(validators.Session),
-  controllers.SessionController.store
+  handle(controllers.SessionController.store)
 )
 
 // usa o authMiddleware em todas as rotas apartir das rotas seguintes
@@ -25,11 +32,19 @@ routes.use(authMiddleware)
 /**
  * Ads
  */
-routes.get('/ads', controllers.AdController.index)
-routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
-routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
-routes.delete('/ads/:id', controllers.AdController.destroy)
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
 /**
  * Purchases
@@ -37,7 +52,7 @@ routes.delete('/ads/:id', controllers.AdController.destroy)
 routes.post(
   '/purchases',
   validate(validators.Purchase),
-  controllers.PurchaseController.store
+  handle(controllers.PurchaseController.store)
 )
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
+// youch é uma biblioteca para renderizar o erro e onde ocorreu o erro
+// ele possui dois tipos de visualização, o toJSON() e o toHTML()
+const Youch = require('youch')
+const validate = require('express-validation')
 const databaseConfig = require('./config/database')
 
 class App {
@@ -10,6 +15,9 @@ class App {
     this.database()
     this.middlewares()
     this.routes()
+
+    // Exception tem que vir depois da configuração das rotas
+    this.exception()
   }
 
   database () {
@@ -25,6 +33,25 @@ class App {
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      // Para visualizar o erro enquanto está em ambiente de desenvolvimento
+      if (process.env.NODE_ENV !== 'production') {
+        const youch = new Youch(err, req)
+
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal Server Error' })
+    })
   }
 }
 module.exports = new App().express
